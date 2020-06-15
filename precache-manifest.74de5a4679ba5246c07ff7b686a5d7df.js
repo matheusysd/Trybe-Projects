d9602dddc93f61032b4e19c251b7a5d2dc265b14
matheusysd <matheusyurisd@gmail.com>
2020-06-15 16:34:27 -0300

    Updates

---
+++ precache-manifest.74de5a4679ba5246c07ff7b686a5d7df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc253b3cdb7e751daf9a07e7b5d0592a",
+    "url": "/Trybe-Projects/sd-04-project-movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "83ab558c4b642eebe799",
+    "url": "/Trybe-Projects/sd-04-project-movie-card-library-crud/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2a692b12ea0456056bef",
+    "url": "/Trybe-Projects/sd-04-project-movie-card-library-crud/static/js/2.189fb365.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trybe-Projects/sd-04-project-movie-card-library-crud/static/js/2.189fb365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ab558c4b642eebe799",
+    "url": "/Trybe-Projects/sd-04-project-movie-card-library-crud/static/js/main.c7e99d5a.chunk.js"
+  },
+  {
+    "revision": "f23c6d27125b0b9e3277",
+    "url": "/Trybe-Projects/sd-04-project-movie-card-library-crud/static/js/runtime-main.32ab71e1.js"
+  }
+]);
